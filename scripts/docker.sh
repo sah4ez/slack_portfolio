051411c44efa72a92ff8a8a12319dcd6dbeecf7a
Alexandr Kozlenkov <sah4ez32@gmail.com>
2017-08-09 16:26:19 +0300

    add docker install script

---
+++ scripts/docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+apt-get remove docker docker-engine docker.io
+apt-get update
+
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install docker-ce
+apt-cache madison docker-ce
+
+# Docker-compose
+
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/loca.bin/docker-compose
+
